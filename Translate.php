b0ef5328b4cd4462121589d249479de76ead520d
Roman Bahatyi <rbagatyi@gmail.com>
2015-01-06 23:51:18 +0200

    Update Translate.php

--- Translate.php
+++ Translate.php
@@ -9,7 +9,7 @@ namespace richweber\google-translate;
 
 /**
  * Yii2 extension for Google Translate API
- * ~~~
+ *
  * @link https://cloud.google.com/translate/v2/using_rest
  * @author Roman Bahatyi <rbagatyi@gmail.com>
  * @since 1.0
