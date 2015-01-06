5cef9c4cba6758812de31cade471af7402f6c331
Roman Bahatyi <rbagatyi@gmail.com>
2015-01-06 23:50:17 +0200

    Create Translate.php

---
+++ Translate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright Copyright &copy; Roman Bahatyi, richweber.net, 2015
+ * @package yii2-google-translate-api
+ * @version 1.0.0
+ */
+
+namespace richweber\google-translate;
+
+/**
+ * Yii2 extension for Google Translate API
+ * ~~~
+ * @link https://cloud.google.com/translate/v2/using_rest
+ * @author Roman Bahatyi <rbagatyi@gmail.com>
+ * @since 1.0
+ */
+class Translate
+{
+    //
+}
